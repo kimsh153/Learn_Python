e5e8835843b44e846df2cc553715d543646c51ed
sunghun <sunghun4777@naver.com>
2021-04-14 21:19:27 +0900

    2021-04-14 study python

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-for i in range(6):
-    for j in range(i):
-        print('*',end='')
-    print(' ')
+for i in range(5):
+    for j in range(5):
+        print('*', end='')
+    print()
+print()
+for i in range(3):
+    for j in range(7):
+        print('*', end='')
+    print()
+print()
+
+for i in range(5):
+    for j in range(5):
+        if j <= i:
+            print('*',end='')
+    print()
+print()
+
+for i in range(5):
+    for j in range(5):
+        if j == i:
+            print('*',end='')
+        else:
+            print(' ',end='')
+    print()
