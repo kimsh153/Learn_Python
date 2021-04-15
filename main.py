5e2518689fd2d8cf794642725d8d639ae8af418c
sunghun <sunghun4777@naver.com>
2021-04-15 19:09:20 +0900

    2021-04-15 파이썬 수업

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
-for i in range(5):
-    for j in range(5):
-        print('*', end='')
-    print()
-print()
-for i in range(3):
-    for j in range(7):
-        print('*', end='')
-    print()
-print()
+#for문 별찍기
+# for i in range(5):
+#     for j in range(5):
+#         print('*', end='')
+#     print()
+# print()
+# for i in range(3):
+#     for j in range(7):
+#         print('*', end='')
+#     print()
+# print()
+#
+# for i in range(5):
+#     for j in range(5):
+#         if j <= i:
+#             print('*',end='')
+#     print()
+# print()
+#
+# for i in range(5):
+#     for j in range(5):
+#         if j == i:
+#             print('*',end='')
+#         else:
+#             print(' ',end='')
+#     print()
+import turtle as t
+t.shape('turtle')
+t.speed('fastest')
 
-for i in range(5):
-    for j in range(5):
-        if j <= i:
-            print('*',end='')
-    print()
-print()
-
-for i in range(5):
-    for j in range(5):
-        if j == i:
-            print('*',end='')
-        else:
-            print(' ',end='')
-    print()
+for i in range(300):
+    t.fd(i)
+    t.right(91)
+t.mainloop()
