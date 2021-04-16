5a50f2def7ec6e437630bfc37f4cabfe06a30f46
sunghun <sunghun4777@naver.com>
2021-04-16 13:10:26 +0900

    2021-04-16 파이썬 공부

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
-#for문 별찍기
-# for i in range(5):
-#     for j in range(5):
-#         print('*', end='')
-#     print()
-# print()
-# for i in range(3):
-#     for j in range(7):
-#         print('*', end='')
-#     print()
-# print()
-#
-# for i in range(5):
-#     for j in range(5):
-#         if j <= i:
-#             print('*',end='')
-#     print()
-# print()
-#
-# for i in range(5):
-#     for j in range(5):
-#         if j == i:
-#             print('*',end='')
-#         else:
-#             print(' ',end='')
-#     print()
 import turtle as t
-t.shape('turtle')
-t.speed('fastest')
 
-for i in range(300):
-    t.fd(i)
-    t.right(91)
-t.mainloop()
+
+# for 별찍기
+def first():
+    for i in range(5):
+        for j in range(5):
+            print('*', end='')
+        print()
+    print()
+    for i in range(3):
+        for j in range(7):
+            print('*', end='')
+        print()
+    print()
+
+    for i in range(5):
+        for j in range(5):
+            if j <= i:
+                print('*', end='')
+        print()
+    print()
+
+    for i in range(5):
+        for j in range(5):
+            if j == i:
+                print('*', end='')
+            else:
+                print(' ', end='')
+        print()
+
+
+# Turtle 그래픽
+def turtle():
+    t.shape('turtle')
+    t.speed('fastest')
+
+    for i in range(300):
+        t.fd(i)
+        t.right(91)
+    t.mainloop()
+
+
+# List
+def list():
+    a = [10, 20, 30, 50, 9]
+    a.append([500, 600])
+    a.extend([500, 600])
+    a.insert(2, 500)
+    a.insert(len(a), 500)
+    a[1:1] = [500, 600]
+    a.pop(1)
+    del a[1]
+    a.reverse()
+    a.sort()
+    print(a)
+    print(len(a))
