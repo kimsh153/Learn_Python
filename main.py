5771199057e40f7f148b96fdb518be992ad79402
sunghun <sunghun4777@naver.com>
2021-04-17 19:31:08 +0900

    2021-04-17 파이썬 공부

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def turtle():
 
 
 # List
-def list():
+def List():
     a = [10, 20, 30, 50, 9]
     a.append([500, 600])
     a.extend([500, 600])
@@ -55,3 +55,43 @@ def list():
     a.sort()
     print(a)
     print(len(a))
+
+
+# 2D List
+def TwoD_List():
+    a = [[10, 20], [30, 40], [50, 60]]
+    print(a[0][0])
+    print(a[0][1])
+    print(a[2][1])
+    for x, y in a:
+        print(x, y)
+    for i in a:
+        for j in i:
+            print(j, end=' ')
+        print()
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(a[i][j], end=' ')
+        print()
+    i = 0
+    while i < len(a):
+        x, y = a[i]
+        print(x, y)
+        i += 1
+    a = []
+    for i in range(3):
+        line = []
+        for j in range(2):
+            line.append(0)
+        a.append(line)
+    a = [[0 for j in range(2)] for i in range(3)]
+    print(a)
+    a = [3, 1, 2, 7, 5]
+    b = []
+    for i in a:
+        line = []
+        for j in range(i):
+            line.append(0)
+        b.append(line)
+    a = [[0] * i for i in [3, 1, 2, 7, 5]]
+    print(a)
