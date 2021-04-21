7882338ffb1728552a28278e0a46491a5eff723d
sunghun <sunghun4777@naver.com>
2021-04-21 19:31:14 +0900

    2021-04-21 파이썬 공부

--- main.py
+++ main.py
@@ -177,4 +177,13 @@ for key in x.keys():
 print()
 for value in x.values():
     print(value, end=' ')
+print()
 keys = ['a', 'b', 'c', 'd']
+x = {key: value for key, value in dict.fromkeys(keys).items()}
+print(x)
+print({key: 0 for key in dict.fromkeys(['a', 'b', 'c', 'd']).keys()})
+print({value: 0 for value in {'a': 10, 'b': 20, 'c': 30, 'd': 40}.values()})
+print({value: key for key, value in {'a': 10, 'b': 20, 'c': 30, 'd': 40}.items()})
+x = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
+x = {key: value for key, value in x.items() if value != 20}
+print(x)
