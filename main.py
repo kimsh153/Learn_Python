3da4be1311726c5247ad1b4fb5fd524d49c81d5c
sunghun <sunghun4777@naver.com>
2021-05-03 07:51:34 +0900

    2021-05-03 파이썬 공부

--- main.py
+++ main.py
@@ -365,3 +365,23 @@ file.close()
 with open('hello.txt', 'r')as file:
     s = file.read()
     print(s)
+with open('hello.txt', 'w')as file:
+    for i in range(3):
+        file.write('Hello, world! {0}\n'.format(i))
+lines = ['안녕하세요.\n', '파이썬\n', '코딩 도장입니다.\n']
+with open('hello.txt', 'w') as file:
+    file.writelines(lines)
+with open('hello.txt', 'r') as file:
+    lines = file.readlines()
+    print(lines)
+with open('hello.txt', 'r') as file:
+    line = None
+    while line != '':
+        line = file.readline()
+        print(line.strip('\n'))
+with open('hello.txt', 'r') as file:
+    for line in file:
+        print(line.strip('\n'))
+file = open('hello.txt', 'r')
+a, b, c = file
+print(a, b, c)
