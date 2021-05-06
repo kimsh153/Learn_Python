fb3b34d3b03c83a5eb30bb063ec5291a85f91746
sunghun <sunghun4777@naver.com>
2021-05-07 08:14:35 +0900

    2021-05-07 파이썬 공부

--- main.py
+++ main.py
@@ -410,40 +410,61 @@ def file():
 
 
 # palindrome
-# word = input('단어를 입력하세요: ')
-# is_palindrome = True
-# for i in range(len(word) // 2):
-#     if word[i] != word[-1 - i]:
-#         is_palindrome = False
-#         break
-#
-# print(is_palindrome)
-# print(word == word[::-1])
-word = 'level'
-print(list(word) == list(reversed(word)))
-print(list(word))
-print(list(reversed(word)))
-print(word == ''.join(reversed(word)))
-print(word)
-print(''.join(reversed(word)))
-text = 'Hello'
-for i in range(len(text) - 1):
-    print(text[i], text[i + 1], sep='')
-text = 'this is python script'
-words = text.split()
-for i in range(len(words) - 1):
-    print(words[i], words[i + 1])
-text = 'hello'
-two_gram = zip(text, text[1:])
-for i in two_gram:
-    print(i[0], i[1], sep='')
-text = 'hello'
-print(list(zip(text, text[1:])))
-text = 'this is python script'
-words = text.split()
-print(list(zip(words, words[1:])))
-text = 'hello'
-print([text[i:] for i in range(3)])
-print(list(zip(['hello', 'ello', 'llo'])))
-print(list(zip(*['hello', 'ello', 'llo'])))
-print(list(zip(*[text[i:] for i in range(3)])))
+def palindrome():
+    word = input('단어를 입력하세요: ')
+    is_palindrome = True
+    for i in range(len(word) // 2):
+        if word[i] != word[-1 - i]:
+            is_palindrome = False
+            break
+
+    print(is_palindrome)
+    print(word == word[::-1])
+    word = 'level'
+    print(list(word) == list(reversed(word)))
+    print(list(word))
+    print(list(reversed(word)))
+    print(word == ''.join(reversed(word)))
+    print(word)
+    print(''.join(reversed(word)))
+    text = 'Hello'
+    for i in range(len(text) - 1):
+        print(text[i], text[i + 1], sep='')
+    text = 'this is python script'
+    words = text.split()
+    for i in range(len(words) - 1):
+        print(words[i], words[i + 1])
+    text = 'hello'
+    two_gram = zip(text, text[1:])
+    for i in two_gram:
+        print(i[0], i[1], sep='')
+    text = 'hello'
+    print(list(zip(text, text[1:])))
+    text = 'this is python script'
+    words = text.split()
+    print(list(zip(words, words[1:])))
+    text = 'hello'
+    print([text[i:] for i in range(3)])
+    print(list(zip(['hello', 'ello', 'llo'])))
+    print(list(zip(*['hello', 'ello', 'llo'])))
+    print(list(zip(*[text[i:] for i in range(3)])))
+
+
+# function
+def hello():
+    print('Hello, world!')
+
+
+def pas():
+    pass
+
+
+def add(a, b):
+    """독스트링 이 함수는 a와 b를 더한 뒤 결과를 변환하는 함수입니다."""
+    return a + b
+
+
+hello()
+x = add(10, 20)
+print(x)
+print(add.__doc__)
