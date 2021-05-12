b984e4af0d99bab0b6f4491619460919007ddcee
sunghun <sunghun4777@naver.com>
2021-05-12 20:21:27 +0900

    2021년 05월 12일 파이썬 공부

--- main.py
+++ main.py
@@ -682,3 +682,41 @@ class Person:
 
 james = Person()
 james.greeting()
+
+
+class Person:
+    def __init__(self, name, age, address):
+        self.hello = '안녕하세요'
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def greeting(self):
+        print('{0} 저는 {1}입니다.'.format(self.hello, self.name))
+
+
+maria = Person('마리아', 20, '서울시 서초구 반포동')
+maria.greeting()
+
+print('이름: ', maria.name)
+print('나이: ', maria.age)
+print('주소: ', maria.address)
+
+
+class Person:
+    def __init__(self, name, age, address, wallet):
+        self.name = name
+        self.age = age
+        self.address = address
+        self.__wallet = wallet
+
+    def pay(self, amount):
+        if amount > self.__wallet:
+            print('돈이 모자라네...')
+            return
+        self.__wallet -= amount
+        print('이제 {0}원 남았네요.'.format(self.__wallet))
+
+
+maria = Person('마리아', 20, '서울시 서초구 반포동', 10000)
+maria.pay(3000)
