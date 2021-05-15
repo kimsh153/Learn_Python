2dbe3a506586f4050521def744c447969d47cf7d
sunghun <sunghun4777@naver.com>
2021-05-15 23:29:21 +0900

    2021년 05월 14일 파이썬 공부

--- main.py
+++ main.py
@@ -814,3 +814,18 @@ james = Person()
 maria = Person()
 
 Person.print_count()
+
+
+class Person:
+    def greeting(self):
+        print('안녕하세요')
+
+
+class Student(Person):
+    def study(self):
+        print('공부하기')
+
+
+james = Student()
+james.greeting()
+james.study()
