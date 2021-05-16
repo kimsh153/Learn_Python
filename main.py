109e717a52e109b420c48e47f8482f38e8a31d1c
sunghun <sunghun4777@naver.com>
2021-05-16 15:35:14 +0900

    2021년 05월 16일 파이썬 공부

--- main.py
+++ main.py
@@ -829,3 +829,63 @@ class Student(Person):
 james = Student()
 james.greeting()
 james.study()
+
+
+class Person:
+    def greeting(self):
+        print('안녕하세요.')
+
+
+class Student(Person):
+    def study(self):
+        print('공부하기')
+
+
+james = Student()
+james.greeting()
+james.study()
+
+
+class Person:
+    def greeting(self):
+        print('안녕하세요.')
+
+
+class PersonList:
+    def __init__(self):
+        self.person_list = []
+
+    def append_person(self, person):
+        self.person_list.append(person)
+
+
+class Person:
+    def __init__(self):
+        print('Perosn __init__')
+        self.hello = '안녕하세요'
+
+
+class Student(Person):
+    def __init__(self):
+        print('Student __init__')
+        super().__init__()  # super(Student, self).__init__()
+        self.school = '파이썬 코딩 도장'
+
+
+james = Student()
+print(james.school)
+print(james.hello)
+
+
+class Person:
+    def __init__(self):
+        print('Person __init__')
+        self.hello = '안녕하세요'
+
+
+class Student(Person):
+    pass
+
+
+james = Student()
+print(james.hello)
