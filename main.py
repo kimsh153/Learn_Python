321a7e22746027a3306076202d3aefc5c2e8ddc7
sunghun <sunghun4777@naver.com>
2021-05-19 16:36:39 +0900

    2021년 05월 19일 파이썬 공부

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import turtle as t
 import copy
 import pickle
+import math
 from abc import *
 
 
@@ -972,3 +973,23 @@ class Student(StudentBase):
 james = Student()
 james.study()
 james.go_to_school()
+
+
+class Point2D:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+p1 = Point2D(x=30, y=20)
+p2 = Point2D(x=60, y=50)
+
+a = p2.x - p1.x
+b = p2.y - p1.y
+print('p1: {} {}'.format(p1.x, p1.y))
+print('p2: {} {}'.format(p2.x, p2.y))
+
+c = math.sqrt((a * a) + (b * b))
+print(c)
+
+
