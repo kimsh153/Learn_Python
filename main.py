d5d4e8ea616e548f848b76b1d96cf64d66bfc6dc
sunghun <sunghun4777@naver.com>
2021-05-21 07:49:38 +0900

    2021년 05월 21일 파이썬 공부

--- main.py
+++ main.py
@@ -964,3 +964,19 @@ except ZeroDivisionError as e:
     print('숫자를 0으로 나눌 수 없습니다.', e)
 except IndexError as e:
     print('잘못된 인덱스입니다.', e)
+try:
+    x = int(input('나눌 숫자를 입력하세요: '))
+    y = 10 / x
+except ZeroDivisionError:
+    print('숫자를 0으로 나눌 수 없습니다')
+else:
+    print(y)
+try:
+    x = int(input('나눌 숫자를 입력하세요: '))
+    y = 10 / x
+except ZeroDivisionError:
+    print('숫자를 0으로 나눌 수 없습니다')
+else:
+    print(y)
+finally:
+    print('코드 실행이 끝났습니다')
