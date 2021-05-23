d0cca3d4c729b10d1de5210bf0157467e98f3e78
sunghun <sunghun4777@naver.com>
2021-05-24 07:52:03 +0900

    2021년 05월 24일 파이썬 공부

--- main.py
+++ main.py
@@ -996,3 +996,27 @@ it = range(3).__iter__()
 print(it.__next__())
 print(it.__next__())
 print(it.__next__())
+class Counter:
+    def __init__(self, stop):
+        self.current = 0
+        self.stop = stop
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current < self.stop:
+            r = self.current
+            self.current += 1
+            return r
+        else:
+            raise StopIteration
+
+
+for i in Counter(3):
+    print(i, end=' ')
+print()
+a, b, c = Counter(3)
+print(a, b, c)
+a, b, c, d, e = Counter(5)
+print(a, b, c, d, e)
