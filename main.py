d012bc0fe63207028593f31436b8fb319e418e0b
sunghun <sunghun4777@naver.com>
2021-06-05 17:29:29 +0900

    2021년 06월 05일 파이썬 공부

--- main.py
+++ main.py
@@ -1323,3 +1323,44 @@ def world():
 
 hello()
 world()
+
+
+def trace(func):
+    def wrapper(a, b):
+        r = func(a, b)
+        print(' {0}(a={1}, b={2}) -> {3}'.format(func.__name__, a, b, r))
+        return r
+
+    return wrapper
+
+
+@trace
+def add(a, b):
+    return a + b
+
+
+print(add(10, 20))
+
+
+def trace(func):
+    def wrapper(*args, **kwargs):
+        r = func(*args, **kwargs)
+        print('{0}(args={1}, kwargs={2}) -> {3}'.format(func.__name__, args, kwargs, r))
+
+        return r
+
+    return wrapper
+
+
+@trace
+def get_max(*args):
+    return max(args)
+
+
+@trace
+def get_min(**kwargs):
+    return min(kwargs.values())
+
+
+print(get_max(10, 20))
+print(get_min(x=10, y=20, z=30))
