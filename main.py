07f5165a1d8e17d0ce732f22be3115fa46ed2f01
sunghun <sunghun4777@naver.com>
2021-06-06 15:06:45 +0900

    2021년 06월 06일 파이썬 공부

--- main.py
+++ main.py
@@ -1364,3 +1364,29 @@ def get_min(**kwargs):
 
 print(get_max(10, 20))
 print(get_min(x=10, y=20, z=30))
+
+
+def is_multiple(x):
+    def real_decorator(func):
+        def wrapper(a, b):
+
+            r = func(a, b)
+
+            if r % x == 0:
+                print('{0}의 반환값은 {1}의 배수입니다.'.format(func.__name__, x))
+            else:
+                print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, x))
+            return r
+
+        return wrapper
+
+    return real_decorator
+
+
+@is_multiple(3)
+def add(a, b):
+    return a + b
+
+
+print(add(10, 20))
+print(add(2, 5))
