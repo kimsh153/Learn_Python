cfd1ed472fd0ce4be109f56c7cc6b938879280c4
sunghun <sunghun4777@naver.com>
2021-06-08 07:53:21 +0900

    2021년 06월 08일 파이썬 공부

--- main.py
+++ main.py
@@ -1408,6 +1408,53 @@ def hello():
 
 
 hello()
+
+
 # @Trace 데코레이터를 안 쓸떄
 # trace_hello = Trace(hello)
 # trace_hello()
+
+
+class Trace:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        r = self.func(*args, **kwargs)
+        print('{0}(args={1}, kwargs={2}) -> {3}'.format(self.func.__name__, args, kwargs, r))
+
+        return r
+
+
+@Trace
+def add(a, b):
+    return a + b
+
+
+print(add(10, 20))
+print(add(a=10, b=20))
+
+
+class IsMultiple:
+    def __init__(self, x):
+        self.x = x
+
+    def __call__(self, func):
+        def wrapper(a, b):
+            r = func(a, b)
+            if r % self.x == 0:
+                print('{0}의 반환값은 {1}의 배수입니다'.format(func.__name__, self.x))
+            else:
+                print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, self.x))
+            return r
+
+        return wrapper
+
+
+@IsMultiple(3)
+def add(a, b):
+    return a + b
+
+
+print(add(10, 20))
+print(add(2, 5))
