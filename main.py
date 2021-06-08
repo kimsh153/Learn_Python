175ce0089d9759d68c3e35e2f06dc14669b84ef4
sunghun <sunghun4777@naver.com>
2021-06-09 08:17:49 +0900

    2021년 06월 09일 파이썬 공부

--- main.py
+++ main.py
@@ -1286,175 +1286,153 @@ def coroutine():
     co.send(None)
 
 
-def hello():
-    print('hello 함수 시작')
-    print('hello')
-    print('hello 함수 끝')
+# 데코레이터
+def decorator():
+    def hello():
+        print('hello 함수 시작')
+        print('hello')
+        print('hello 함수 끝')
 
+    def world():
+        print('world 함수 시작')
+        print('world')
+        print('world 함수 끝')
 
-def world():
-    print('world 함수 시작')
-    print('world')
-    print('world 함수 끝')
+    hello()
+    world()
 
+    def trace(func):
+        def wrapper():
+            print(func.__name__, '함수 시작')
+            func()
+            print(func.__name__, '함수 끝')
 
-hello()
-world()
+        return wrapper
 
+    @trace
+    def hello():
+        print('hello')
 
-def trace(func):
-    def wrapper():
-        print(func.__name__, '함수 시작')
-        func()
-        print(func.__name__, '함수 끝')
+    @trace
+    def world():
+        print('world')
 
-    return wrapper
+    hello()
+    world()
 
-
-@trace
-def hello():
-    print('hello')
-
-
-@trace
-def world():
-    print('world')
-
-
-hello()
-world()
-
-
-def trace(func):
-    def wrapper(a, b):
-        r = func(a, b)
-        print(' {0}(a={1}, b={2}) -> {3}'.format(func.__name__, a, b, r))
-        return r
-
-    return wrapper
-
-
-@trace
-def add(a, b):
-    return a + b
-
-
-print(add(10, 20))
-
-
-def trace(func):
-    def wrapper(*args, **kwargs):
-        r = func(*args, **kwargs)
-        print('{0}(args={1}, kwargs={2}) -> {3}'.format(func.__name__, args, kwargs, r))
-
-        return r
-
-    return wrapper
-
-
-@trace
-def get_max(*args):
-    return max(args)
-
-
-@trace
-def get_min(**kwargs):
-    return min(kwargs.values())
-
-
-print(get_max(10, 20))
-print(get_min(x=10, y=20, z=30))
-
-
-def is_multiple(x):
-    def real_decorator(func):
+    def trace(func):
         def wrapper(a, b):
-
             r = func(a, b)
-
-            if r % x == 0:
-                print('{0}의 반환값은 {1}의 배수입니다.'.format(func.__name__, x))
-            else:
-                print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, x))
+            print(' {0}(a={1}, b={2}) -> {3}'.format(func.__name__, a, b, r))
             return r
 
         return wrapper
 
-    return real_decorator
+    @trace
+    def add(a, b):
+        return a + b
 
+    print(add(10, 20))
 
-@is_multiple(3)
-def add(a, b):
-    return a + b
+    def trace(func):
+        def wrapper(*args, **kwargs):
+            r = func(*args, **kwargs)
+            print('{0}(args={1}, kwargs={2}) -> {3}'.format(func.__name__, args, kwargs, r))
 
-
-print(add(10, 20))
-print(add(2, 5))
-
-
-class Trace:
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self):
-        print(self.func.__name__, '함수 시작')
-        self.func()
-        print(self.func.__name__, '함수 끝')
-
-
-@Trace
-def hello():
-    print('hello')
-
-
-hello()
-
-
-# @Trace 데코레이터를 안 쓸떄
-# trace_hello = Trace(hello)
-# trace_hello()
-
-
-class Trace:
-    def __init__(self, func):
-        self.func = func
-
-    def __call__(self, *args, **kwargs):
-        r = self.func(*args, **kwargs)
-        print('{0}(args={1}, kwargs={2}) -> {3}'.format(self.func.__name__, args, kwargs, r))
-
-        return r
-
-
-@Trace
-def add(a, b):
-    return a + b
-
-
-print(add(10, 20))
-print(add(a=10, b=20))
-
-
-class IsMultiple:
-    def __init__(self, x):
-        self.x = x
-
-    def __call__(self, func):
-        def wrapper(a, b):
-            r = func(a, b)
-            if r % self.x == 0:
-                print('{0}의 반환값은 {1}의 배수입니다'.format(func.__name__, self.x))
-            else:
-                print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, self.x))
             return r
 
         return wrapper
 
+    @trace
+    def get_max(*args):
+        return max(args)
 
-@IsMultiple(3)
-def add(a, b):
-    return a + b
+    @trace
+    def get_min(**kwargs):
+        return min(kwargs.values())
+
+    print(get_max(10, 20))
+    print(get_min(x=10, y=20, z=30))
+
+    def is_multiple(x):
+        def real_decorator(func):
+            def wrapper(a, b):
+
+                r = func(a, b)
+
+                if r % x == 0:
+                    print('{0}의 반환값은 {1}의 배수입니다.'.format(func.__name__, x))
+                else:
+                    print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, x))
+                return r
+
+            return wrapper
+
+        return real_decorator
+
+    @is_multiple(3)
+    def add(a, b):
+        return a + b
+
+    print(add(10, 20))
+    print(add(2, 5))
+
+    class Trace:
+        def __init__(self, func):
+            self.func = func
+
+        def __call__(self):
+            print(self.func.__name__, '함수 시작')
+            self.func()
+            print(self.func.__name__, '함수 끝')
+
+    @Trace
+    def hello():
+        print('hello')
+
+    hello()
+
+    # @Trace 데코레이터를 안 쓸떄
+    # trace_hello = Trace(hello)
+    # trace_hello()
+
+    class Trace:
+        def __init__(self, func):
+            self.func = func
+
+        def __call__(self, *args, **kwargs):
+            r = self.func(*args, **kwargs)
+            print('{0}(args={1}, kwargs={2}) -> {3}'.format(self.func.__name__, args, kwargs, r))
+
+            return r
+
+    @Trace
+    def add(a, b):
+        return a + b
+
+    print(add(10, 20))
+    print(add(a=10, b=20))
+
+    class IsMultiple:
+        def __init__(self, x):
+            self.x = x
+
+        def __call__(self, func):
+            def wrapper(a, b):
+                r = func(a, b)
+                if r % self.x == 0:
+                    print('{0}의 반환값은 {1}의 배수입니다'.format(func.__name__, self.x))
+                else:
+                    print('{0}의 반환값은 {1}의 배수가 아닙니다'.format(func.__name__, self.x))
+                return r
+
+            return wrapper
+
+    @IsMultiple(3)
+    def add(a, b):
+        return a + b
+
+    print(add(10, 20))
+    print(add(2, 5))
 
 
-print(add(10, 20))
-print(add(2, 5))
